d81afacce3d85ac288cb6764c2eeb487ad09891e
Robert Lavery <rob@robertlavery.com>
2023-09-22 12:57:50 -0500

    Argument passing

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ mod config;
 use monster::Monster;
 use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = std::env::args().collect();
-    let file_path = args.get(1).unwrap();
+    let dollar_zero = &args[0]; // deserves to panic
+    let file_path = args.get(1).unwrap_or_else(|| {
+        eprintln!("Usage: {} FILE", dollar_zero);
+        process::exit(1);
+    });
     let contents = fs::read_to_string(file_path)?;
     let config = config::build(&contents)?;
     let my_monster = Monster::build(config.monsters)?;
