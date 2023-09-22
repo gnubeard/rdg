847b30fa8e28020eb801a95bcf66aa9dc34348f0
Robert Lavery <rob@robertlavery.com>
2023-09-22 12:51:18 -0500

    CLI argument now

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ mod config;
 
 use monster::Monster;
 use std::error::Error;
-//use std::env; //TODO args and env vars
 use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string("/home/rlavery/.dg.toml")?; //FIXME
+    let args: Vec<String> = std::env::args().collect();
+    let file_path = args.get(1).unwrap();
+    let contents = fs::read_to_string(file_path)?;
     let config = config::build(&contents)?;
     let my_monster = Monster::build(config.monsters)?;
     println!("{}", my_monster);
