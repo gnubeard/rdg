28c19727d1c76a74febd1f4269649de1c577e446
Robert Lavery <rob@robertlavery.com>
2023-09-23 19:54:22 -0500

    There's the robots

--- src/threat.rs
+++ src/threat.rs
@@ -14,21 +14,24 @@ pub enum Threat {
 
 impl Threat {
     pub fn roll(config: &Config, threat_type_roll: u8) -> Result<Self, Box<dyn Error>> {
-        let mut rng = thread_rng();
         let threat = match threat_type_roll {
             0..=1 => Threat::Monster(Monster::build(&config.monsters)?),
-            2..=3 => Threat::Hazard(
-                config
-                    .rooms
-                    .hazards
-                    .choose(&mut rng)
-                    .ok_or("No hazards found!")?
-                    .to_string(),
-            ),
-            2..=6 => Threat::build_person(&config.people)?,
+            2..=3 => Threat::build_person(&config.people)?,
+            4..=5 => Threat::build_robot(&config.people)?,
+            6..=6 => Threat::build_hazard(&config.rooms)?,
             _ => panic!("d6 rolled higher than a 6!"),
         };
         Ok(threat)
+    }
+    fn build_hazard(room_attrs: &RoomAttributes) -> Result<Self, Box<dyn Error>> {
+        let mut rng = thread_rng();
+        let h = room_attrs
+            .hazards
+            .choose(&mut rng)
+            .ok_or("No hazards found!")?
+            .to_string();
+        let hazard = Threat::Hazard(h);
+        Ok(hazard)
     }
     fn build_person(people_attrs: &PeopleAttributes) -> Result<Self, Box<dyn Error>> {
         let mut rng = thread_rng();
@@ -42,8 +45,23 @@ impl Threat {
             .choose(&mut rng)
             .ok_or("No last names found!")?
             .to_string();
-        let threat = Threat::Person(format!("{} {}", first_name, last_name));
-        Ok(threat)
+        let person = Threat::Person(format!("{} {}", first_name, last_name));
+        Ok(person)
+    }
+    fn build_robot(robot_attrs: &PeopleAttributes) -> Result<Self, Box<dyn Error>> {
+        let mut rng = thread_rng();
+        let prefix = robot_attrs
+            .robot_prefixes
+            .choose(&mut rng)
+            .ok_or("No prefixes found!")?
+            .to_string();
+        let suffix = robot_attrs
+            .robot_suffixes
+            .choose(&mut rng)
+            .ok_or("No suffixes found!")?
+            .to_string();
+        let person = Threat::Robot(format!("{}-{}", prefix, suffix));
+        Ok(person)
     }
 }
 
